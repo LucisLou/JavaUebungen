63110b7bf058735b2056d49819d889a27edade60
Lou <83348079+LucisLou@users.noreply.github.com>
2021-07-19 14:39:42 +0200

    Making of SimpleCalculations.java, fulfilling the task.

---
+++ src/Uebung1/SimpleCalculations.java
@@ -0,0 +1,25 @@
+package Uebung1;
+
+public class SimpleCalculations {
+
+    public static void main(String[] args) {
+
+        int firstNum = 1337;
+
+        double secondNum = 2442.2442;
+
+        System.out.println("Addition: " + firstNum + " + " + secondNum + " = " + (firstNum + secondNum));
+        System.out.println("Subtraction: " + firstNum + " - " + secondNum + " = " + (firstNum - secondNum));
+        System.out.println("Multiplication: " + firstNum + " x " + secondNum + " = " + (firstNum * secondNum));
+        System.out.println("Division: " + firstNum + " + " + secondNum + " = " + (firstNum / secondNum));
+
+        double modResult = firstNum%secondNum;
+
+        System.out.printf("Modulo: %d mod %f = %f", firstNum, secondNum, modResult);
+
+        /*printf is a formatted system print with placeholders. %d is a placeholder format for integers and long
+            %f is a placeholder for double*/
+
+    }
+
+}
