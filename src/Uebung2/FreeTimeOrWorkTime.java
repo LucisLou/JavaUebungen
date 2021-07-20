2a3eac59602ad975ecf7fd6a4397628652557489
Lou <83348079+LucisLou@users.noreply.github.com>
2021-07-20 09:02:10 +0200

    Initial creation of FreeTimeOrWorkTime.java, also fulfilling the task.

---
+++ src/Uebung2/FreeTimeOrWorkTime.java
@@ -0,0 +1,30 @@
+package Uebung2;
+
+import java.util.Scanner;
+
+public class FreeTimeOrWorkTime {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Hey, what time is it now?%nGive the time in the 24h format in full integers (e.g. ... 9 10 ...):%n");
+
+        int time = scanner.nextInt();
+
+        String isWorkTime = switch (time) {
+            case 8, 9, 10, 11, 14, 15, 16:
+                yield "It's still work time!";
+            case 12:
+                yield "It's lunch break!";
+            case 13:
+                yield "Lunch break just ended.. Back to work!";
+            case 1, 2, 3, 4, 5, 6, 7, 17, 18, 19, 20, 21, 22, 23, 24:
+                yield "It's not work time! Hooray!";
+            default:
+                throw new IllegalStateException("Unexpected value: " + time);
+        }; //Because this is a statement, it's ended with a semi-colon
+
+        System.out.printf("It's %d o'clock! Which means: %s", time, isWorkTime);
+    }
+}
