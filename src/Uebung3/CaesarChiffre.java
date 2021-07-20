82f71163ed30a9dfbddef2dacdbc3a4f56e8cbf1
Lou <83348079+LucisLou@users.noreply.github.com>
2021-07-20 13:43:33 +0200

    Creation of CaesarChiffre.java. Completing the task

---
+++ src/Uebung3/CaesarChiffre.java
@@ -0,0 +1,52 @@
+package Uebung3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class CaesarChiffre {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Give in your text to encrypt in English:");
+
+        String text = scanner.nextLine();
+        int key = random.nextInt((25 - 1) +1) + 1; //generate a random number between 1 and 26 using random.nextInt((max - min) + 1) + min;
+        String encryptedText = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            int z = text.charAt(i);
+            char indexChar = text.charAt(i);
+
+            if (z >= 65 && z <= 90) { //Only upper case letters
+
+                z = z + key; // encryption
+
+                if (z > 90) {
+                    z -= 26; // -26 to start again at A because there are 26 letters in the English alphabet
+                }
+
+                indexChar = (char)z;
+
+            } else if (z >= 97 && z <= 122) { //only lower case letters
+
+                z = z + key;
+
+                if (z > 122) {
+                    z -= 26;
+                }
+
+                indexChar = (char)z;
+
+            }
+
+            encryptedText += indexChar; //writing the encrypted text by adding all the encrypted characters to the string
+        }
+
+        System.out.printf("%s%nKey generated for encryption: %d", encryptedText, key);
+
+    }
+
+}
