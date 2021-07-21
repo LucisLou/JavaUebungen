a751d913a848d2b19b90a878b6a6707aaf391f2d
Lou <83348079+LucisLou@users.noreply.github.com>
2021-07-21 08:52:13 +0200

    Initial. Creation of MaximaCalculation.java, completing the task.

---
+++ src/Uebung3/MaximaCalculation.java
@@ -0,0 +1,40 @@
+package Uebung3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class MaximaCalculation {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> userNumbers = new ArrayList<Integer>();
+
+        System.out.println("Please insert your numbers you wish to calculate the maximum of. Insert q to quit:");
+
+        String userInput = "";
+
+        int inputNumberConvert;
+
+        while (!userInput.equals("q")) { // equals() instead of == for Strings
+            userInput = scanner.nextLine();
+
+            if (!userInput.equals("q")) {
+                try { //when you need to check if an input would create an exception error, use try catch
+                    inputNumberConvert = Integer.valueOf(userInput);
+                    userNumbers.add(inputNumberConvert);
+                } catch (Exception e) { //catches error
+                    System.out.println("Invalid input");
+                    System.out.println(e.getMessage());// Prints out message why the error happened
+                }
+            }
+        }
+
+        int max = Collections.max(userNumbers);
+        System.out.printf("The maximum of your numbers is: %d", max);
+
+    }
+
+}
