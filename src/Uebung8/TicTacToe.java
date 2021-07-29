bb600a6864a44a1c50c41e3896a26576bf4b9426
Lou <83348079+LucisLou@users.noreply.github.com>
2021-07-29 14:15:19 +0200

    Removed scanner in main, as it's not needed.

--- src/Uebung8/TicTacToe.java
+++ src/Uebung8/TicTacToe.java
@@ -8,7 +8,6 @@ public class TicTacToe {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         char[][] tttField = {
