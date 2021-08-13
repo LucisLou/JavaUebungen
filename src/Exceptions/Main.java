7f1e79d29fde07fff12fb19b1f4ef518845b4544
Lou <83348079+LucisLou@users.noreply.github.com>
2021-08-13 11:19:37 +0200

    Initial creation of Main.java

--- src/Exceptions/Main.java
+++ src/Exceptions/Main.java
@@ -1,4 +1,17 @@
 package Exceptions;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            UserManager newUser1 = new UserManager("Amelia", "Turner", "none", -3);
+        } catch (InvalidAgeException e) {
+            e.printStackTrace();
+            System.out.println("Age is not valid");
+        } finally {
+            System.out.println("User was allocated regardless if valid or not.");
+        }
+        System.out.println("Message to prove program still runs");
+    }
 }
