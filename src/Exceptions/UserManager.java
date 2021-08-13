c3f4688cbaaf168835197d23c001a15c5cb50b1f
Lou <83348079+LucisLou@users.noreply.github.com>
2021-08-13 11:19:15 +0200

    Initial creation of UserManager.java and the exception class InvalidAgeException.java

--- src/Exceptions/UserManager.java
+++ src/Exceptions/UserManager.java
@@ -6,4 +6,20 @@ public class UserManager {
     private String firstName;
     private String lastName;
     private String title;
+    private int age;
+
+    public UserManager (String firstName, String lastname, String title, int age) throws InvalidAgeException{
+
+        this.firstName = firstName;
+        this.lastName = lastname;
+        this.title = title;
+
+        if (age < 0 || age > 120) {
+            throw new InvalidAgeException("Age is invalid.");
+        }
+        this.age = age;
+
+        USER_COUNT++;
+
+    }
 }
